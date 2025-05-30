2c35aab1b03f2174d84d7fd948099c9afe314b90
Moamal-2000 <moamalalaapro1@gmail.com>
2025-05-30 07:16:17 +0300

    feat: prepare ArtCard with props, jsx structure and CSS selectors

--- src/Components/ArtCard/ArtCard.jsx
+++ src/Components/ArtCard/ArtCard.jsx
@@ -1,7 +1,24 @@
+import Image from "next/image";
 import s from "./ArtCard.module.scss";
 
-const ArtCard = () => {
-  return <article className={s.artCard}></article>;
+const ArtCard = ({ data }) => {
+  const { title, description, image, codepenUrl } = data;
+
+  return (
+    <article className={s.artCard}>
+      <div className={s.frame}>
+        <Image src={image} alt={title} width={300} height={300} />
+      </div>
+
+      <h2 className={s.title}>{title}</h2>
+      <p className={s.description}>{description}</p>
+
+      <a href={codepenUrl} target="_blank" rel="noreferrer">
+        {/* Codepen icon */}
+        View on CodePen
+      </a>
+    </article>
+  );
 };
 
 export default ArtCard;

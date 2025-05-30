abcc7ff5acb64630b0453a44cd1baa7e22238dc2
Moamal-2000 <moamalalaapro1@gmail.com>
2025-05-30 07:04:51 +0300

    feat: implement introduction with its styles

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,17 @@
 import s from "./page.module.scss";
 
 export default function Home() {
-  return <main className={s.home}></main>;
+  return (
+    <main className={s.home}>
+      <div className="container">
+        <header className={s.header}>
+          <h1 className={s.title}>CSS Art Gallery</h1>
+          <p className={s.description}>
+            A collection of creative artwork made with pure CSS by{" "}
+            <a href="https://www.linkedin.com/feed">Moamal Alaa</a>
+          </p>
+        </header>
+      </div>
+    </main>
+  );
 }

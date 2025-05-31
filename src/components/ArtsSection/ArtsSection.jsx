703ff4652d28c26793d05f08aef500ba2695b6dc
Moamal-2000 <moamalalaapro1@gmail.com>
2025-05-31 04:23:46 +0300

    init: ArtsSection component & sample of CSS arts data

---
+++ src/components/ArtsSection/ArtsSection.jsx
@@ -0,0 +1,7 @@
+import s from "./ArtsSection.module.scss";
+
+const ArtsSection = () => {
+  return <section className={s.artsSection}></section>;
+};
+
+export default ArtsSection;
---
+++ src/data/cssArtsData.js
@@ -0,0 +1,11 @@
+export const CSS_ARTS_DATA = [
+  {
+    title: "CSS Art #1",
+    description: "CSS Art #1 Description",
+    image: "",
+    urlData: {
+      type: "Github",
+      url: "",
+    },
+  },
+];

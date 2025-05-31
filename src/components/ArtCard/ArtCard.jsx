5b24dfdd3d2c1f138f29052d0b65b5e98141a895
Moamal-2000 <moamalalaapro1@gmail.com>
2025-05-31 04:20:54 +0300

    update(ArtCard): render icon/text based on the type of the URL dynamically

--- src/components/ArtCard/ArtCard.jsx
+++ src/components/ArtCard/ArtCard.jsx
@@ -1,8 +1,11 @@
 import Image from "next/image";
+import CodePenIcon from "../Svgs/CodePenIcon";
+import GitHubIcon from "../Svgs/GitHubIcon";
 import s from "./ArtCard.module.scss";
 
 const ArtCard = ({ data }) => {
-  const { title, description, image, codepenUrl } = data;
+  const { title, description, image, urlData } = data;
+  const isGithubUrl = urlData.type.toLowerCase() === "github";
 
   return (
     <article className={s.artCard}>
@@ -13,9 +16,9 @@ const ArtCard = ({ data }) => {
       <h2 className={s.title}>{title}</h2>
       <p className={s.description}>{description}</p>
 
-      <a href={codepenUrl} target="_blank" rel="noreferrer">
-        {/* Codepen icon */}
-        View on CodePen
+      <a href={urlData.url} target="_blank" rel="noreferrer">
+        {isGithubUrl ? <GitHubIcon /> : <CodePenIcon />}
+        View on {urlData.type}
       </a>
     </article>
   );

559cac8e381c46aaa4ebc460e64d5eb832e18e9d
Moamal-2000 <moamalalaapro1@gmail.com>
2025-05-31 09:03:12 +0300

    update: add descriptions to each CSS draw

--- src/data/cssArtsData.js
+++ src/data/cssArtsData.js
@@ -1,7 +1,8 @@
 export const CSS_ARTS_DATA = [
   {
     title: "Animated Polygon",
-    description: "CSS Art #1 Description",
+    description:
+      "A sleek animated polygon featuring a centered letter, designed as a minimal and elegant loading indicator. Inspired by Brittany Chiang's portfolio (v4), this loader combines clean geometry with smooth animation to create a refined and modern visual element.",
     image: "/css-arts/animated-polygon.png",
     urlData: {
       type: "CodePen",
@@ -11,7 +12,8 @@ export const CSS_ARTS_DATA = [
   },
   {
     title: "Audio Cassette",
-    description: "CSS Art #2 Description",
+    description:
+      "A retro-inspired audio cassette brought to life with vibrant, shifting border colors. The motion gives it a lively, energetic feel like it's ready to play your favorite mixtape.",
     image: "/css-arts/audio-cassette.png",
     urlData: {
       type: "GitHub",
@@ -21,7 +23,8 @@ export const CSS_ARTS_DATA = [
   },
   {
     title: "Camera",
-    description: "CSS Art #2 Description",
+    description:
+      "Just a fun little camera I made for laughs. you can “snap” a photo with a flash effect that simulates a real camera, and get a cheeky emoji reaction to see how handsome you are. It’s playful, light-hearted, and doesn’t take itself too seriously!",
     image: "/css-arts/camera.png",
     urlData: {
       type: "GitHub",
@@ -32,7 +35,8 @@ export const CSS_ARTS_DATA = [
 
   {
     title: "Desktop",
-    description: "CSS Art #2 Description",
+    description:
+      "A simple yet detailed desktop scene featuring a PC screen, mouse, desk lamp, and a red coffee cube, capturing the essentials of a cozy and productive workspace. Made just for fun, it brings a bit of everyday comfort to life with clean lines and subtle details.",
     image: "/css-arts/desktop.png",
     urlData: {
       type: "CodePen",
@@ -42,7 +46,8 @@ export const CSS_ARTS_DATA = [
   },
   {
     title: "404 Page",
-    description: "CSS Art #2 Description",
+    description:
+      "A playful 404 error page featuring a cute little guy blinking endlessly from inside the zero. It adds a touch of charm and personality to an otherwise frustrating moment making lost pages a bit more enjoyable.",
     image: "/css-arts/error-404-page.png",
     urlData: {
       type: "GitHub",
@@ -52,7 +57,8 @@ export const CSS_ARTS_DATA = [
   },
   {
     title: "Frown Face",
-    description: "CSS Art #2 Description",
+    description:
+      "A quirky frown-faced character whose eyes continuously scan left to right without stopping. A fun little animation that adds personality and a bit of humor to a simple expression.",
     image: "/css-arts/frown-face.png",
     urlData: {
       type: "CodePen",
@@ -62,7 +68,8 @@ export const CSS_ARTS_DATA = [
   },
   {
     title: "Writing Paper",
-    description: "CSS Art #2 Description",
+    description:
+      "a wrinkled paper where you can write down anything that comes to mind and when you’re ready, just hit the Save button to keep your text safe in localStorage. It’s like having a little note on the screen that’s always there when you come back.",
     image: "/css-arts/paper.png",
     urlData: {
       type: "CodePen",
@@ -72,7 +79,8 @@ export const CSS_ARTS_DATA = [
   },
   {
     title: "Space Cat",
-    description: "CSS Art #2 Description",
+    description:
+      "A big cat appears from behind a planet, watching you with curiosity. The scene includes soft clouds and a glowing crescent moon, combining a cosmic atmosphere with a playful touch.",
     image: "/css-arts/space-cat.png",
     urlData: {
       type: "CodePen",
@@ -82,7 +90,8 @@ export const CSS_ARTS_DATA = [
   },
   {
     title: "Tricky Frame",
-    description: "CSS Art #2 Description",
+    description:
+      "This tricky shape was a challenge I set for myself while learning HTML and CSS. It wasn’t easy to build, but I pushed through and finally nailed it. It was a little personal victory that shows how practice pays off.",
     image: "/css-arts/tricky-frame.png",
     urlData: {
       type: "CodePen",
@@ -92,7 +101,8 @@ export const CSS_ARTS_DATA = [
   },
   {
     title: "UNO Cards",
-    description: "CSS Art #2 Description",
+    description:
+      "A vibrant set of four UNO cards featuring numbers 1 to 9 in all the classic colors. It took me quite some time to finish, but I had a lot of fun creating them while practicing HTML & CSS",
     image: "/css-arts/uno-cards.png",
     urlData: {
       type: "GitHub",

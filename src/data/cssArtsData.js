0838039979c82e1feb0a61d030f61d123ed35000
Moamal-2000 <moamalalaapro1@gmail.com>
2025-05-31 04:44:34 +0300

    update: add rest CSS arts data

--- src/data/cssArtsData.js
+++ src/data/cssArtsData.js
@@ -9,4 +9,95 @@ export const CSS_ARTS_DATA = [
     },
     id: 1,
   },
+  {
+    title: "Audio Cassette",
+    description: "CSS Art #2 Description",
+    image: "/css-arts/audio-cassette.png",
+    urlData: {
+      type: "GitHub",
+      url: "https://moamal-2000.github.io/music-time",
+    },
+    id: 2,
+  },
+  {
+    title: "Camera",
+    description: "CSS Art #2 Description",
+    image: "/css-arts/camera.png",
+    urlData: {
+      type: "GitHub",
+      url: "https://moamal-2000.github.io/camera",
+    },
+    id: 3,
+  },
+
+  {
+    title: "Desktop",
+    description: "CSS Art #2 Description",
+    image: "/css-arts/desktop.png",
+    urlData: {
+      type: "CodePen",
+      url: "https://codepen.io/moamal-2000/pen/XWyLBbe",
+    },
+    id: 4,
+  },
+  {
+    title: "404 Page",
+    description: "CSS Art #2 Description",
+    image: "/css-arts/error-404-page.png",
+    urlData: {
+      type: "GitHub",
+      url: "https://moamal-2000.github.io/error-404-page",
+    },
+    id: 5,
+  },
+  {
+    title: "Frown Face",
+    description: "CSS Art #2 Description",
+    image: "/css-arts/frown-face.png",
+    urlData: {
+      type: "CodePen",
+      url: "https://codepen.io/moamal-2000/pen/XWPBpov",
+    },
+    id: 6,
+  },
+  {
+    title: "Writing Paper",
+    description: "CSS Art #2 Description",
+    image: "/css-arts/paper.png",
+    urlData: {
+      type: "CodePen",
+      url: "https://codepen.io/moamal-2000/pen/rNQRBwv",
+    },
+    id: 7,
+  },
+  {
+    title: "Space Cat",
+    description: "CSS Art #2 Description",
+    image: "/css-arts/space-cat.png",
+    urlData: {
+      type: "CodePen",
+      url: "https://codepen.io/moamal-2000/pen/YzRwyyJ",
+    },
+    id: 8,
+  },
+  {
+    title: "Tricky Frame",
+    description: "CSS Art #2 Description",
+    image: "/css-arts/tricky-frame.png",
+    urlData: {
+      type: "CodePen",
+      url: "https://codepen.io/moamal-2000/pen/dywZvoY",
+    },
+    id: 9,
+  },
+  {
+    title: "UNO Cards",
+    description: "CSS Art #2 Description",
+    image: "/css-arts/uno-cards.png",
+    urlData: {
+      type: "GitHub",
+      url: "https://moamal-2000.github.io/UNO-cards",
+    },
+    id: 10,
+  },
 ];

9ac7819bd6281d748862704e76bb0d9e8ef890f6
Moamal-2000 <moamalalaapro1@gmail.com>
2025-06-04 03:40:44 +0300

    refactor(ArtCard): use ArtCardContent component

--- src/components/ArtCard/ArtCard.jsx
+++ src/components/ArtCard/ArtCard.jsx
@@ -3,13 +3,11 @@
 import { fadeInOnViewMotionProps } from "@/functions/motionConfig";
 import { motion } from "motion/react";
 import Image from "next/image";
-import CodePenIcon from "../Svgs/CodePenIcon";
-import GitHubIcon from "../Svgs/GitHubIcon";
 import s from "./ArtCard.module.scss";
+import ArtCardContent from "./ArtCardContent/ArtCardContent";
 
 const ArtCard = ({ data, index }) => {
-  const { title, description, image, urlData, priority } = data;
-  const isGithubUrl = urlData.type.toLowerCase() === "github";
+  const { title, image, urlData, priority } = data;
 
   return (
     <motion.article
@@ -38,44 +36,7 @@ const ArtCard = ({ data, index }) => {
         />
       </motion.a>
 
-      <div className={s.content}>
-        <motion.h2
-          className={s.title}
-          {...fadeInOnViewMotionProps({
-            visibilityThreshold: 0,
-            initialY: 5,
-            delay: index * 0.05 + 0.3,
-          })}
-        >
-          {title}
-        </motion.h2>
-        <motion.p
-          className={s.description}
-          {...fadeInOnViewMotionProps({
-            visibilityThreshold: 0,
-            initialY: 5,
-            delay: index * 0.05 + 0.4,
-          })}
-        >
-          {description}
-        </motion.p>
-
-        <div className={s.links}>
-          <motion.a
-            href={urlData.url}
-            className={s.link}
-            target="_blank"
-            rel="noreferrer"
-            initial={{ opacity: 0 }}
-            whileInView={{ opacity: 1 }}
-            viewport={{ once: true }}
-            transition={{ duration: 0.4, delay: index * 0.05 + 0.5 }}
-          >
-            {isGithubUrl ? <GitHubIcon /> : <CodePenIcon />}
-            View on {urlData.type}
-          </motion.a>
-        </div>
-      </div>
+      <ArtCardContent data={data} />
     </motion.article>
   );
 };
--- src/components/ArtCard/ArtCardContent/ArtCardContent.jsx
+++ src/components/ArtCard/ArtCardContent/ArtCardContent.jsx
@@ -1,9 +1,53 @@
-import s from './ArtCardContent.module.scss'
+import CodePenIcon from "@/components/Svgs/CodePenIcon";
+import GitHubIcon from "@/components/Svgs/GitHubIcon";
+import { fadeInOnViewMotionProps } from "@/functions/motionConfig";
+import { motion } from "motion/react";
+import s from "./ArtCardContent.module.scss";
 
-const ArtCardContent = () => {
+const ArtCardContent = ({ data }) => {
+  const { title, description, urlData, index } = data;
+  const isGithubUrl = urlData.type.toLowerCase() === "github";
+
   return (
-    <div>ArtCardContent</div>
-  )
-}
+    <div className={s.content}>
+      <motion.h2
+        className={s.title}
+        {...fadeInOnViewMotionProps({
+          visibilityThreshold: 0,
+          initialY: 5,
+          delay: index * 0.05 + 0.3,
+        })}
+      >
+        {title}
+      </motion.h2>
+      <motion.p
+        className={s.description}
+        {...fadeInOnViewMotionProps({
+          visibilityThreshold: 0,
+          initialY: 5,
+          delay: index * 0.05 + 0.4,
+        })}
+      >
+        {description}
+      </motion.p>
 
-export default ArtCardContent
+      <div className={s.links}>
+        <motion.a
+          href={urlData.url}
+          className={s.link}
+          target="_blank"
+          rel="noreferrer"
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          viewport={{ once: true }}
+          transition={{ duration: 0.4, delay: index * 0.05 + 0.5 }}
+        >
+          {isGithubUrl ? <GitHubIcon /> : <CodePenIcon />}
+          View on {urlData.type}
+        </motion.a>
+      </div>
+    </div>
+  );
+};
+
+export default ArtCardContent;
